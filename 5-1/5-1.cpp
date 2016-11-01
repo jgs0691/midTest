801f674b38c081b7c19df28999aba12af5fc65ca
jgs0691 <jgs6086@daum.net>
2016-11-01 15:12:46 +0900

    6

---
+++ 5-1/5-1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int i = 0, sum = 0, mult = 1;
+
+	while (++i <= 5) {
+		if (i % 2)
+			sum += i;
+		else
+			mult *= i;
+	}
+	printf("합 : %d 곱 : %d\n", sum, mult);
+
+	return 0;
+}
